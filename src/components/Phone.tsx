69829743c4d47a992069b3b62c2305376e6457e4
Sagar Yadav <sagardxd5@gmail.com>
2024-05-09 23:30:21 +0530

    hero page

---
+++ src/components/Phone.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils"
+import { HtmlHTMLAttributes } from "react"
+
+interface PhoneProps extends HtmlHTMLAttributes<HTMLDivElement> {
+    imgSrc: string
+    dark?: boolean
+}
+
+const Phone = ({ className, imgSrc, dark = false, ...props }: PhoneProps) => {
+    return (
+        <div className={cn("relative pointer-events-none z-50 overflow-hidden",
+            className)}
+            {...props}>
+            <img
+                src={dark ? "/phone-template-dark-edges.png" :
+                    "/phone-template-white-edges.png"}
+                alt="phone image"
+                className="pointer-events-none z-50 select-none"
+            />
+            <div className="absolute -z-10 inset-0">
+                <img
+                    className="object-cover min-w-full min-h-full"
+                    src={imgSrc}
+                    alt="overlay-phone-img"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Phone
